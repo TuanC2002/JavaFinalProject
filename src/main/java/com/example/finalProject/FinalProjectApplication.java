8d140fcd02c3e1c3b06aa4a376a62f9f4f090823
ducdanglambai <phuocducbui9@gmail.com>
2023-07-08 10:20:09 +0700

    Update FinalProjectApplication.java

--- src/main/java/com/example/finalProject/FinalProjectApplication.java
+++ src/main/java/com/example/finalProject/FinalProjectApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class FinalProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProjectApplication.class, args);
 	}
